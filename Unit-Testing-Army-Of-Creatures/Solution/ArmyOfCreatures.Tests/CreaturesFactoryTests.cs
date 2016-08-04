32484de906b3dca8522585729c4b422f7a69d64e
susanok <suzanaboyuklieva@abv.bg>
2016-08-04 14:39:32 +0300

    End with testing Extended Creatures Factory

---
+++ Unit-Testing-Army-Of-Creatures/Solution/ArmyOfCreatures.Tests/CreaturesFactoryTests.cs
@@ -0,0 +1,61 @@
+﻿namespace ArmyOfCreatures.Tests
+{
+    using Extended;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class CreaturesFactoryTests
+    {
+        [TestCase("Angel")]
+        [TestCase("Archangel")]
+        [TestCase("ArchDevil")]
+        [TestCase("Behemoth")]
+        [TestCase("Devil")]
+        [TestCase("Goblin")]
+        [TestCase("WolfRaider")]
+        [TestCase("Griffin")]
+        [TestCase("CyclopsKing")]
+        
+        public void ExtendedCreaturesFactory_WhenValidNameIsPassed_ShouldReturnExpectedType(string name)
+        {
+            //Arrange
+            var factory = new ExtendedCreaturesFactory();
+
+            //Act
+            var creature = factory.CreateCreature(name);
+
+            //Assert
+            Assert.AreEqual(name,
+                creature.GetType().Name, "Creature is not initialized properly");
+        }
+
+        [Test]
+
+        public void ExtendedCreaturesFactory_WhenInValidNameIsPassed_ShouldThrowArgumentException()
+        {
+            //Arrange
+            var factory = new ExtendedCreaturesFactory();
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => factory.CreateCreature(""));
+        }
+
+        [Test]
+
+        public void ExtendedCreaturesFactory_WhenInValidNameIsPassed_ShouldThrowArgumentExceptionWithExpectedMessage()
+        {
+            var factory = new ExtendedCreaturesFactory();
+
+            try
+            {
+                factory.CreateCreature(""); 
+            }
+            catch (ArgumentException exception)
+            {
+
+                Assert.AreEqual($"Invalid creature type \"\"!", exception.Message, "Exception's message is not correct");
+            }
+        }
+    }
+}
