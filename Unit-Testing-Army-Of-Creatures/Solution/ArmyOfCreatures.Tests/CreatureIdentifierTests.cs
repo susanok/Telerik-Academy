1c8ee1690db7aa8b82aa42b94a95e65f1288cd32
susanok <suzanaboyuklieva@abv.bg>
2016-08-04 17:04:26 +0300

    Some tests for Creature Identifier

---
+++ Unit-Testing-Army-Of-Creatures/Solution/ArmyOfCreatures.Tests/CreatureIdentifierTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ArmyOfCreatures.Tests
+{
+    using Logic.Battles;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class CreatureIdentifierTests
+    {
+        [Test]
+        public void CreatureIdentifierFromString_WhenNullValueIsPassed_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => CreatureIdentifier.CreatureIdentifierFromString(null));
+        }
+
+        [Test]
+        public void CreatureIdentifierFromString_WhenInvalidValueToParseIntIsPassed_ShouldThrowFormatException()
+        {
+            Assert.Throws<FormatException>(() => CreatureIdentifier.CreatureIdentifierFromString("Angel(test)"));
+        }
+
+        [Test]
+        public void CreatureIdentifierFromString_WhenInvalidValueIsPassed_ShouldThrowIndexOutOfRangeException()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => CreatureIdentifier.CreatureIdentifierFromString("Angel"));
+        }
+
+        [Test]
+        public void CreatureIdentifierFromString_WhenValueIsPassed_ShouldReturnExpectedType()
+        {
+            var identifier = CreatureIdentifier.CreatureIdentifierFromString("Angel(1)");
+
+            Assert.IsInstanceOf<CreatureIdentifier>(identifier);
+        }
+
+    }
+}
