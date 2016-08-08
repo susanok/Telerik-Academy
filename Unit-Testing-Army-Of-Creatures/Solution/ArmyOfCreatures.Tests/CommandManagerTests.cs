425eb55419284450ae7187b51eea349c0ab29a3e
susanok <suzanaboyuklieva@abv.bg>
2016-08-08 12:55:56 +0300

    Some Other Tests

---
+++ Unit-Testing-Army-Of-Creatures/Solution/ArmyOfCreatures.Tests/CommandManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using ArmyOfCreatures.Console.Commands;
+using ArmyOfCreatures.Logic;
+using ArmyOfCreatures.Logic.Battles;
+using ArmyOfCreatures.Logic.Creatures;
+using ArmyOfCreatures.Tests.MockedClasses;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace ArmyOfCreatures.Tests
+{
+    [TestFixture]
+    public class CommandManagerTests
+    {
+        [Test]
+        public void ProcessCommand_WhenInvalidCommandLineIsPassed_ShouldThrowArgumentNullException()
+        {
+            var commandManager = new CommandManager();
+            var mockedFactory = new Mock<ICreaturesFactory>();
+            var mockedLogger = new Mock<ILogger>();
+
+            var battleManager = new MockedBattleManager(mockedFactory.Object, mockedLogger.Object);
+
+            Assert.Throws<ArgumentNullException>(() => commandManager.ProcessCommand(null, battleManager));
+        }
+    }
+}
---
+++ Unit-Testing-Army-Of-Creatures/Solution/ArmyOfCreatures.Tests/CreaturesInBattleTests.cs
@@ -0,0 +1,26 @@
+﻿using ArmyOfCreatures.Logic;
+using ArmyOfCreatures.Logic.Battles;
+using ArmyOfCreatures.Logic.Creatures;
+using ArmyOfCreatures.Tests.MockedClasses;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace ArmyOfCreatures.Tests
+{
+    [TestFixture]
+    public class CreaturesInBattleTests
+    {
+        [Test]
+        public void CreaturesInBattleDealDamage_WhenInvalidDefenderIsPassed_ShouldThrowArgumentNullException()
+        {
+            var creature = new Angel();
+            var creaturesInBattle = new CreaturesInBattle(creature, 1);
+
+            Assert.Throws<ArgumentNullException>(() => creaturesInBattle.DealDamage(null));
+        }
+
+        //TODO: other tests
+    }
+}
