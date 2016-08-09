e59e653869288b6195326180b6b619b9255be083
susanok <suzanaboyuklieva@abv.bg>
2016-08-09 04:24:29 +0300

    Some Products tests

---
+++ Cosmetics-Shop-Unit-Testing/CosmeticsShop/Cosmetics.Tests/Products/ShampooTests.cs
@@ -0,0 +1,34 @@
+﻿using Cosmetics.Common;
+using Cosmetics.Engine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests.Products
+{
+    [TestFixture]
+    public class ShampooTests
+    {
+        [Test]
+        public void Print_WhenItIsInvoked_ShouldReturnStringWithAShampooDetailsInRequiredFormat()
+        {
+            var factory = new CosmeticsFactory();
+
+            var shampoo = factory.CreateShampoo("Cool", "Nivea", 10M, GenderType.Men, 500, UsageType.EveryDay);
+
+            var expectedResult = new StringBuilder();
+            expectedResult.AppendLine("- Nivea - Cool:");
+            expectedResult.AppendLine("  * Price: $5000");
+            expectedResult.AppendLine("  * For gender: Men");
+            expectedResult.AppendLine("  * Quantity: 500 ml");
+            expectedResult.Append("  * Usage: EveryDay");
+
+            var actualResult = shampoo.Print();
+
+            Assert.AreEqual(expectedResult.ToString(), actualResult);
+        }
+    }
+}
---
+++ Cosmetics-Shop-Unit-Testing/CosmeticsShop/Cosmetics.Tests/Products/ToothpasteTests.cs
@@ -0,0 +1,33 @@
+﻿using Cosmetics.Common;
+using Cosmetics.Engine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests.Products
+{
+    [TestFixture]
+    public class ToothpasteTests
+    {
+        [Test]
+        public void Print_WhenItIsInvoked_ShouldReturnStringWithAToothpasteDetailsInRequiredFormat()
+        {
+            var factory = new CosmeticsFactory();
+
+            var toothPaste = factory.CreateToothpaste("Cool", "Nivea", 10M, GenderType.Men, new List<string>() { "fluor", "golqma"});
+
+            var expectedResult = new StringBuilder();
+            expectedResult.AppendLine("- Nivea - Cool:");
+            expectedResult.AppendLine("  * Price: $10");
+            expectedResult.AppendLine("  * For gender: Men");
+            expectedResult.AppendFormat("  * Ingredients: {0}", String.Join(", ", new List<string>() { "fluor", "golqma" }));
+
+            var actualResult = toothPaste.Print();
+
+            Assert.AreEqual(expectedResult.ToString(), actualResult);
+        }
+    }
+}
