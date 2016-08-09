28e65f1c23f1057442508a690bb8629a3e0f6914
susanok <suzanaboyuklieva@abv.bg>
2016-08-09 03:53:10 +0300

    Cosmetics Factory tests

--- Cosmetics-Shop-Unit-Testing/CosmeticsShop/Cosmetics.Tests/Engine/CosmeticsEngineTests.cs
+++ Cosmetics-Shop-Unit-Testing/CosmeticsShop/Cosmetics.Tests/Engine/CosmeticsEngineTests.cs
@@ -146,38 +146,29 @@ namespace Cosmetics.Tests.Engine
             var commandName = "ShowCategory";
             var categoryName = "ForMale";
 
-            //mock the objects for the engine constructor
             var mockedFactory = new Mock<ICosmeticsFactory>();
             var mockedShoppingCart = new Mock<IShoppingCart>();
             var mockedParser = new Mock<ICommandParser>();
 
-            //mock the objects that needs to process the command
             var mockedCategory = new Mock<ICategory>();
             var mockedCommand = new Mock<ICommand>();
 
-            // setup the fake command getters to return correct things
             mockedCommand.SetupGet(x => x.Name).Returns(commandName);
             mockedCommand.SetupGet(x => x.Parameters).Returns(new List<string>() { categoryName });
             mockedCategory.SetupGet(x => x.Name).Returns(categoryName);
 
-            //setup the ReadCommands method
-            //behaviour to returns a list with the mocked command
             mockedParser.Setup(x => x.ReadCommands())
                 .Returns(new List<ICommand>() { mockedCommand.Object });
 
-            //mock the engine
             var engine = new MockedCosmeticsEngine(
                 mockedFactory.Object,
                 mockedShoppingCart.Object,
                 mockedParser.Object);
 
-            //add the category in the list of categories
             engine.Categories.Add(categoryName, mockedCategory.Object);
 
-            //Act
             engine.Start();
 
-            //check that print is called
             mockedCategory.Verify(x => x.Print(), Times.Once);
 
         }
@@ -193,16 +184,13 @@ namespace Cosmetics.Tests.Engine
             var shampooMililiters = "500";
             var shampooUsage = "everyday";
 
-            //mock the objects for the engine constructor
             var mockedFactory = new Mock<ICosmeticsFactory>();
             var mockedShoppingCart = new Mock<IShoppingCart>();
             var mockedParser = new Mock<ICommandParser>();
 
-            //mock the objects that needs to process the command
             var mockedShampoo = new Mock<IShampoo>();
             var mockedCommand = new Mock<ICommand>();
 
-            // setup the fake command getters to return correct things
             mockedCommand.SetupGet(x => x.Name).Returns(commandName);
             mockedCommand.SetupGet(x => x.Parameters)
                 .Returns(new List<string>() {
@@ -216,21 +204,17 @@ namespace Cosmetics.Tests.Engine
 
             mockedFactory.Setup(x => x.CreateShampoo(shampooName, shampoBrand, 0.50M, GenderType.Men, 500, UsageType.EveryDay))
                 .Returns(mockedShampoo.Object);
-            //setup the ReadCommands method
-            //behaviour to returns a list with the mocked command
+
             mockedParser.Setup(x => x.ReadCommands())
                 .Returns(new List<ICommand>() { mockedCommand.Object });
 
-            //mock the engine
             var engine = new MockedCosmeticsEngine(
                 mockedFactory.Object,
                 mockedShoppingCart.Object,
                 mockedParser.Object);
 
-            //Act
             engine.Start();
 
-            //check that shampoo is added
             Assert.IsTrue(engine.Products.ContainsKey(shampooName));
             Assert.AreSame(mockedShampoo.Object, engine.Products[shampooName]);  
         }
@@ -238,7 +222,6 @@ namespace Cosmetics.Tests.Engine
         [Test]
         public void Start_CreateToothpasteInputIsValid_ShouldBeAddedTheNewToothPasteToTheListOfProducts()
         {
-            //CreateToothpaste White+ Colgate 15.50 men fluor,bqla,golqma
             var commandName = "CreateToothpaste";
             var pasteName = "White+";
             var pasteBrand = "Colgate";
@@ -321,35 +304,27 @@ namespace Cosmetics.Tests.Engine
             var commandName = "RemoveFromShoppingCart";
             var productName = "Cool";
 
-            //mock objects for the engine constructor
             var mockedFactory = new Mock<ICosmeticsFactory>();
             var mockedShoppingCart = new Mock<IShoppingCart>();
             var mockedParser = new Mock<ICommandParser>();
 
-            //mock the fake command and the fake product
             var mockedCommand = new Mock<ICommand>();
             var mockedShampoo = new Mock<IShampoo>();
 
-            //setup mocked command name and parameters to return expected things
             mockedCommand.SetupGet(x => x.Name).Returns(commandName);
             mockedCommand.SetupGet(x => x.Parameters).Returns(new List<string>() { productName });
 
-            //setup the fake product name to return expected name
             mockedShampoo.SetupGet(x => x.Name).Returns(productName);
 
-            //setup the mocked parser who should reads the commands and returns list with the mocked command
             mockedParser.Setup(x => x.ReadCommands()).Returns(new List<ICommand>() { mockedCommand.Object });
 
-            //mock the engine
             var engine = new MockedCosmeticsEngine(
                 mockedFactory.Object,
                 mockedShoppingCart.Object,
                 mockedParser.Object);
 
-            //add the mocked product to the mocked engine
             engine.Products.Add(productName, mockedShampoo.Object);
 
-            //add the mocked product to the mocked shopping cart
             mockedShoppingCart.Setup(x => x.ContainsProduct(mockedShampoo.Object)).Returns(true);
 
             engine.Start();
---
+++ Cosmetics-Shop-Unit-Testing/CosmeticsShop/Cosmetics.Tests/Engine/CosmeticsFactoryTests.cs
@@ -0,0 +1,155 @@
+﻿using Cosmetics.Common;
+using Cosmetics.Contracts;
+using Cosmetics.Engine;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests.Engine
+{
+    [TestFixture]
+    public class CosmeticsFactoryTests
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        public void CreateShampoo_WhenPassedNameIsNullOrEmpty_ShouldThrowNullReferenceException(string name)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<NullReferenceException>(() => factory.CreateShampoo(name, "Cool", 0.50M, GenderType.Men, 500, UsageType.EveryDay));
+        }
+
+        [TestCase("as")]
+        [TestCase("too long name for shampoo")]
+        public void CreateShampoo_WhenPassedNameIsTooLongOrShort_ShouldThrowIndexOutOfRangeException(string name)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateShampoo(name, "Cool", 0.50M, GenderType.Men, 500, UsageType.EveryDay));
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void CreateShampoo_WhenPassedBrandIsNullOrEmpty_ShouldThrowNullReferenceException(string brand)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<NullReferenceException>(() => factory.CreateShampoo("White+", brand, 0.50M, GenderType.Men, 500, UsageType.EveryDay));
+        }
+
+        [TestCase("a")]
+        [TestCase("too long brand for shampoo")]
+        public void CreateShampoo_WhenPassedBrandIsTooLongOrShort_ShouldThrowIndexOutOfRangeException(string brand)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateShampoo("White+", brand, 0.50M, GenderType.Men, 500, UsageType.EveryDay));
+        }
+
+        
+        [Test]
+        public void CreateShampoo_WhenPassedParamsAreValid_ShouldReturnNewShampoo()
+        {
+            var factory = new CosmeticsFactory();
+
+            var shampoo = factory.CreateShampoo("Nivea", "Cool", 0.50M, GenderType.Men, 500, UsageType.EveryDay);
+
+            Assert.IsInstanceOf<IShampoo>(shampoo);
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void CreateCategory_WhenPassedBrandIsNullOrEmpty_ShouldThrowNullReferenceException(string name)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<NullReferenceException>(() => factory.CreateCategory(name));
+        }
+
+        [TestCase("a")]
+        [TestCase("too long name for category")]
+        public void CreateCategory_WhenPassedNameIsTooLongOrShort_ShouldThrowIndexOutOfRangeException(string name)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateCategory(name));
+        }
+
+        [Test]
+        public void CreateCategory_WhenPassedParamsAreValid_ShouldReturnNewCategory()
+        {
+            var factory = new CosmeticsFactory();
+
+            var category = factory.CreateCategory("Nivea");
+
+            Assert.IsInstanceOf<ICategory>(category);
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void CreateToothpaste_WhenPassedNameIsNullOrEmpty_ShouldThrowNullReferenceException(string name)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<NullReferenceException>(() => factory.CreateToothpaste(name, "White+", 15.50M, GenderType.Men, new List<string>() { "fluor", "bqla" }));
+        }
+
+        [TestCase("as")]
+        [TestCase("too long name for toothpaste")]
+        public void CreateToothpaste_WhenPassedNameIsTooLongOrShort_ShouldThrowIndexOutOfRangeException(string name)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateToothpaste(name, "White+", 15.50M, GenderType.Men, new List<string>() { "fluor", "bqla" }));
+
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void CreateToothpaste_WhenPassedBrandIsNullOrEmpty_ShouldThrowNullReferenceException(string brand)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<NullReferenceException>(() => factory.CreateToothpaste("White+", brand, 15.50M, GenderType.Men, new List<string>() { "fluor", "bqla" }));
+        }
+
+        [TestCase("a")]
+        [TestCase("too long brand for toothpaste")]
+        public void CreateToothpaste_WhenPassedBrandIsTooLongOrShort_ShouldThrowIndexOutOfRangeException(string brand)
+        {
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateToothpaste("White+", brand, 15.50M, GenderType.Men, new List<string>() { "fluor", "bqla" }));
+
+        }
+
+        [TestCase("as,fluor,golqma")]
+        [TestCase("golqma,too long brand for toothpaste,bqla")]
+        [TestCase("golqma,bqla,too long brand for toothpaste")]
+        [TestCase("too long brand for toothpaste,bqla,as")]
+        public void CreateToothpaste_WhenPassedIngredientsIsTooLongOrShort_ShouldThrowIndexOutOfRangeException(string listInput)
+        {
+            var ingredients = listInput.Split(',').ToList();
+
+            var factory = new CosmeticsFactory();
+
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateToothpaste("White+", "Nivea", 15.50M, GenderType.Men, ingredients));
+
+        }
+
+        [Test]
+        public void CreateShoppingCart_WhenCreateShoppingCart_ShouldAlwaysReturnNewShoppingCart()
+        {
+            var factory = new CosmeticsFactory();
+
+            var shoppingCart = factory.CreateShoppingCart();
+
+            Assert.IsInstanceOf<IShoppingCart>(shoppingCart);
+        }
+
+    }
+}
