17cc3431eef15c582489cb27fd64cf055a4dab45
susanok <suzanaboyuklieva@abv.bg>
2016-10-28 16:33:04 +0300

    C# OOP army of creatures exam solution

---
+++ C# OOP/Previous Exams/Army of Creatures/Solution/ArmyOfCreatures/Extended/ExtendedBattleManager.cs
@@ -0,0 +1,46 @@
+﻿using ArmyOfCreatures.Logic;
+using ArmyOfCreatures.Logic.Battles;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArmyOfCreatures.Extended
+{
+    public class ExtendedBattleManager : BattleManager, IBattleManager
+    {
+        private readonly ICollection<ICreaturesInBattle> thirdArmyCreatures;
+
+        public ExtendedBattleManager(ICreaturesFactory creaturesFactory, ILogger logger) : base(creaturesFactory, logger)
+        {
+            this.thirdArmyCreatures = new List<ICreaturesInBattle>();
+        }
+
+        public ExtendedBattleManager(ExtendedCreaturesFactory creaturesFactory, ILogger logger)
+            :base(creaturesFactory, logger)
+        {
+        }
+
+        protected override void AddCreaturesByIdentifier(CreatureIdentifier creatureIdentifier, ICreaturesInBattle creaturesInBattle)
+        {
+            if (creatureIdentifier.ArmyNumber == 3)
+            {
+                this.thirdArmyCreatures.Add(creaturesInBattle);
+            }
+            else
+            {
+                base.AddCreaturesByIdentifier(creatureIdentifier, creaturesInBattle);
+            }
+        }
+
+        protected override ICreaturesInBattle GetByIdentifier(CreatureIdentifier identifier)
+        {
+            if (identifier.ArmyNumber == 3)
+            {
+                return this.thirdArmyCreatures.FirstOrDefault(x => x.Creature.GetType().Name == identifier.CreatureType);
+            }
+            else
+            {
+                return base.GetByIdentifier(identifier);
+            }
+        }
+    }
+}
