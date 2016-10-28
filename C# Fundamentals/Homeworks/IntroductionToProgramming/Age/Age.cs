f56b0bf9ddc4c71f59afac1db69e85449da6fabb
susanok <suzanaboyuklieva@abv.bg>
2016-10-28 14:25:06 +0300

    All tasks for the first homework in C# fundamentals course

---
+++ C# Fundamentals/Homeworks/IntroductionToProgramming/Age/Age.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Age
+    {
+        static void Main()
+        {
+            string[] readedBirthDate = Console.ReadLine().Split('.');
+            string userInput = String.Join("-", readedBirthDate);
+            DateTime userDate = DateTime.ParseExact(userInput, "MM-dd-yyyy", null);
+            DateTime today = DateTime.Today;
+            int age = today.Year - userDate.Year;
+
+            if (userDate.Month > today.Month)
+            {
+                age--;
+            }
+            else
+            {
+                if (userDate.Month == today.Month)
+                {
+                    if (userDate.Day > today.Day)
+                    {
+                        age--;
+                    }
+                }
+            }
+
+            Console.WriteLine(age);
+            int futureAge = age + 10;
+            Console.WriteLine(futureAge);
+        }
+    }
+}
---
+++ C# Fundamentals/Homeworks/IntroductionToProgramming/SquareRoot/SquareRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            double numSq = Math.Sqrt(12345);
+            Console.WriteLine(numSq);
+        }
+    }
+}
